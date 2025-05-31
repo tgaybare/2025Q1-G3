8a7fff5f7dcdca075cf2bef8ec942c966224db94
tgaybare <tomigayba02yt@gmail.com>
2025-05-31 16:31:23 -0300

    feat: implement routing with Home and Dashboard pages

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,44 +1,18 @@
 // App.js
 
-import { useAuth } from "react-oidc-context";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import {HomePage} from "../pages/Home";
+import {DashboardPage} from "../pages/Dashboard";
 
 function App() {
-  const auth = useAuth();
-
-  const signOutRedirect = () => {
-    const clientId = "539qi4hbt6km9hd52u1mhrvl24";
-    const logoutUri = "<logout uri>";
-    const cognitoDomain = "https://<user pool domain>";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-  };
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Encountering error... {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
     return (
-        <div>
-          <pre> Hello: {auth.user?.profile.email} </pre>
-          <pre> ID Token: {auth.user?.id_token} </pre>
-          <pre> Access Token: {auth.user?.access_token} </pre>
-          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
-          <button onClick={() => auth.removeUser()}>Sign out</button>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+            </Routes>
+        </BrowserRouter>
     );
-  }
-
-  return (
-      <div>
-        <button onClick={() => auth.signinRedirect()}>Sign in</button>
-        <button onClick={() => signOutRedirect()}>Sign out</button>
-      </div>
-  );
 }
 
 export default App;
