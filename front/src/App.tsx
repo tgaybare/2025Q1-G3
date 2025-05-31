80d4f9f3a058cd005806f0a586ee6a8523a67a61
tgaybare <tomigayba02yt@gmail.com>
2025-05-31 15:54:40 -0300

    feat: integrate OIDC authentication and update app structure

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,34 +1,44 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import Login from './pages/login';
-import Dashboard from './pages/dashboard';
-import React from 'react';
-import { Amplify } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+// App.js
 
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1', // your region
-    userPoolId: 'us-east-1_XXXXXXXXX',
-    userPoolWebClientId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-    authenticationFlowType: 'USER_PASSWORD_AUTH',
-  },
-});
+import { useAuth } from "react-oidc-context";
 
-function App({ signOut, user }: any) {
+function App() {
+  const auth = useAuth();
+
+  const signOutRedirect = () => {
+    const clientId = "539qi4hbt6km9hd52u1mhrvl24";
+    const logoutUri = "<logout uri>";
+    const cognitoDomain = "https://<user pool domain>";
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+  };
+
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Encountering error... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
+        <div>
+          <pre> Hello: {auth.user?.profile.email} </pre>
+          <pre> ID Token: {auth.user?.id_token} </pre>
+          <pre> Access Token: {auth.user?.access_token} </pre>
+          <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+          <button onClick={() => auth.removeUser()}>Sign out</button>
+        </div>
+    );
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+      <div>
+        <button onClick={() => auth.signinRedirect()}>Sign in</button>
+        <button onClick={() => signOutRedirect()}>Sign out</button>
+      </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
