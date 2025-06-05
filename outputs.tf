f62fc3490854a64ff9913d4bd71cee7da1fa6102
tgaybare <tomigayba02yt@gmail.com>
2025-06-04 23:45:25 -0300

    removed unnecessary outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,7 @@
-output "react_app_bucket_name" {
-  value = module.react_app_bucket.bucket_name
-}
 
 output "react_app_bucket_website_url" {
   value       = module.react_app_bucket.website_url
   description = "URL to access the React app"
-}
-
-output "cognito_user_pool_id" {
-  value = module.cognito.user_pool_id
-}
-
-output "cognito_client_id" {
-  value = module.cognito.client_id
 }
 
 output "SlaveServer1_ip" {
